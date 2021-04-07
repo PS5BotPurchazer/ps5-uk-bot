0beed1ade8af163bace0415d6efb7a07c6713a01
PS5BotPurchazer <82070984+PS5BotPurchazer@users.noreply.github.com>
2021-04-07 03:34:45 +0100

    Update Smyths Bot - disk.py

--- disk ps5 bots/Smyths Bot - disk.py
+++ disk ps5 bots/Smyths Bot - disk.py
@@ -5,7 +5,7 @@ import winsound
 
 print("This program comes with ABSOLUTELY NO WARRANTY; for details check included license. This is free software, and you are welcome to redistribute it under certain conditions; check license for conditions.")
 
-item = 'https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259' # Variable for the item wanted
+item = 'https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259' # Variable for the item wanted
 options = Options()
 options.add_argument("user-data-dir=C:\\Users\\ps5\\AppData\\Local\\Google\\Chrome\\User Data")
 driver = webdriver.Chrome('chromedriver.exe', options=options)
@@ -18,6 +18,6 @@ while True:
         driver.get(item) # If the atc button cannot be found, refresh until it is found
     else:
         time.sleep(1)
-        driver.get('https://www.smythstoys.com/uk/en-gb/checkout/multi/payment-method/add') # Goes to checkout page after adding to cart
+        driver.get('https://www.smythstoys.com/ie/en-ie/login/checkout') # Goes to checkout page after adding to cart
         break
 winsound.Beep(899, 5000) # Makes a beep sound for 5 seconds to notify the user that the item is in stock and at checkout page
